40f93f6d3e3cf43865a9ea00df7dd57c18a016cb
Repon Kumar Roy <repon.roy@hotmail.com>
2019-08-10 23:17:42 +0800

    Implement templateHandler to compile and load HTML files Removed hardcoded HTML from go files to separate HTML files

--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte(`
-		<html>
-		<head>
-			<title>Chat</title>
-		</head>
-		<body>
-			Let's chat
-		</body>
-		</html>
-		`))
-	})
+type templateHandler struct {
+	once     sync.Once
+	fileName string
+	templ    *template.Template
+}
 
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.fileName)))
+	})
+	t.templ.Execute(w, nil)
+}
+func main() {
+	//root
+	http.Handle("/", &templateHandler{fileName: "chat.html"})
 	//start the server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
